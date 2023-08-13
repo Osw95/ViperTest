66234a17ffc6ac7b8cf46e889b141630c371d8a9
Oswaldo Ferral <oswaldoferral@gmail.com>
2023-08-13 12:13:03 -0600

    People Entity

--- ViperTest/ViperTest/Entitys/People.swift
+++ ViperTest/ViperTest/Entitys/People.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+
+struct peopleList:Codable{
+    var results:[people]
+}
+
+struct people:Codable{
+    let score:String
+    let pesron:personInfo?
+}
+
+struct personInfo:Codable{
+    let id:Int
+    let url:String
+    let name:String
+    let country:countryInfo?
+    let birthday:String?
+    let deathday:String?
+    let gender:String
+    let image:imageInfo?
+}
+
+struct countryInfo:Codable{
+    let name:String?
+    let code:String?
+    let timezone:String?
+}
+
+struct imageInfo:Codable{
+    let medium:String?
+    let original:String?
+}
